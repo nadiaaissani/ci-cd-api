fd048561bdbd33d9aa04eeb9c2b616d4ee097855
nadiadev <aissaninadia2024@gmail.com>
2026-02-13 11:51:57 +0100

    feat: ajout route produits et resolveJsonModule

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import products from './data/products.json';
 
 const app = express();
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
@@ -11,9 +12,13 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-// Route
+// Route principale
 app.get('/', (req, res) => {
   res.json({ message: 'welcome to CI-CD-API' });
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
 });
 
 app.listen(PORT, () => {
