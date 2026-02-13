876fb20cf5fdcc39053ba51279a342c9950751c9
nadiadev <aissaninadia2024@gmail.com>
2026-02-13 11:32:27 +0100

    chore: ajout de prettier et husky pre-commit

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,21 @@
-
 import dotenv from 'dotenv';
 dotenv.config();
-// Test CI pipeline
-import express, { Request, Response } from 'express';
+
+import express from 'express';
 
 const app = express();
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
 
 // Route Health
-app.get('/health', (req: Request, res: Response) => {
-    res.json({ status: 'ok' });
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
 });
 
 // Route
-app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'welcome to CI-CD-API' });
+app.get('/', (req, res) => {
+  res.json({ message: 'welcome to CI-CD-API' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
